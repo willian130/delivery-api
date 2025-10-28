9ca0ffd8af9c7ddb03f793e420d5f2d6f8664298
willian130 <willianbatista791@gmail.com>
2025-10-28 19:38:55 -0300

    Primeiro commit

---
+++ src/main/java/com/delivery_api/Projeto/Deliverey/API/ProjetoDelivereyApiApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.Deliverey.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDelivereyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDelivereyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery_api/Projeto/Deliverey/API/ProjetoDelivereyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto.Deliverey.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDelivereyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
